4143a901d629ba4c849d4616f18ae33df6cc1ac2
mohitrajsuryavamshi <149007397+mohitrajsuryavamshi@users.noreply.github.com>
2025-06-04 11:16:51 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$VERCEL_GIT_COMMIT_REF" = "main" ]; then
+  echo "✅ Deploying branch: $VERCEL_GIT_COMMIT_REF"
+  exit 1  # Proceed with deployment
+else
+  echo "⛔ Skipping deployment for branch: $VERCEL_GIT_COMMIT_REF"
+  exit 0  # Skip deployment
+fi
